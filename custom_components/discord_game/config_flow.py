592fdf69eb484bd9e54f66dcbed603f2d6235949
SimenAsphaug <simenasphaug@hotmail.com>
2024-06-12 22:57:40 +0200

    test

--- custom_components/discord_game/config_flow.py
+++ custom_components/discord_game/config_flow.py
@@ -7,13 +7,18 @@ from homeassistant import config_entries
 from homeassistant.const import CONF_ACCESS_TOKEN
 from homeassistant.helpers import selector
 from nextcord import LoginFailure
-from .const import DOMAIN
+from .const import DOMAIN, CONF_MEMBERS, CONF_CHANNELS, CONF_IMAGE_FORMAT
 
 _LOGGER = logging.getLogger(__name__)
 
 AUTH_SCHEMA = vol.Schema(
     {
-        vol.Required(CONF_ACCESS_TOKEN): cv.string
+        vol.Required(CONF_ACCESS_TOKEN): cv.string,
+        vol.Required(CONF_IMAGE_FORMAT, default='webp'): selector.SelectSelector(
+            selector.SelectSelectorConfig(options=['png', 'webp', 'jpeg', 'jpg'],
+                                          multiple=False,
+                                          mode=selector.SelectSelectorMode.DROPDOWN),
+        ),
     }
 )
 
@@ -28,21 +33,59 @@ class DiscordGameConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             except ValueError:
                 errors["base"] = "auth"
             if not errors:
-                return self.async_create_entry(title="Discord Game", data=user_input)
+                self.data = user_input
+                self.data[CONF_MEMBERS] = []
+                self.data[CONF_CHANNELS] = []
+
+                return await self.async_step_members()
 
         return self.async_show_form(
             step_id="user", data_schema=AUTH_SCHEMA, errors=errors
         )
 
+    async def async_step_members(self, user_input: Optional[Dict[str, Any]] = None):
+        _MEMBERS_SCHEMA = vol.Schema(
+            {
+                vol.Optional(CONF_MEMBERS): selector.SelectSelector(
+                    selector.SelectSelectorConfig(options=userNames,
+                                                  multiple=True,
+                                                  mode=selector.SelectSelectorMode.DROPDOWN),
+                ),
+                vol.Optional(CONF_CHANNELS): selector.SelectSelector(
+                    selector.SelectSelectorConfig(options=channelNames,
+                                                  multiple=True,
+                                                  mode=selector.SelectSelectorMode.DROPDOWN),
+                ),
+            }
+        )
+
+        errors: Dict[str, str] = {}
+        if user_input is not None:
+            for user in user_input.get(CONF_MEMBERS, []):
+                self.data[CONF_MEMBERS].append(members.get(user).id)
+            if user_input.get(CONF_CHANNELS):
+                for channel in user_input.get(CONF_CHANNELS, []):
+                    self.data[CONF_CHANNELS].append(channels.get(channel).id)
+
+            return self.async_create_entry(title="Discord Game", data=self.data)
+
+        return self.async_show_form(
+            step_id="members", data_schema=_MEMBERS_SCHEMA, errors=errors
+        )
+
     async def validate_auth_and_fetch_data(self, token: str) -> None:
+        global client, members, userNames, channels, channelNames
         client = nextcord.Client(intents=nextcord.Intents.all())
+        members = {}
+        userNames = []
+        channels = {}
+        channelNames = []
+
         try:
             await client.login(token)
             guilds = await client.fetch_guilds().flatten()
             _LOGGER.debug("guilds: %s", guilds)
 
-            members = {}
-            userNames = []
             for guild in guilds:
                 _members = await guild.fetch_members().flatten()
                 for member in _members:
@@ -50,8 +93,6 @@ class DiscordGameConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             userNames = list(dict.fromkeys([member.name for member in members.values()]))
             _LOGGER.debug("userNames: %s", userNames)
 
-            channels = {}
-            channelNames = []
             for guild in guilds:
                 _channels = await guild.fetch_channels()
                 for channel in _channels:
