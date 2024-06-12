a86a15e5c007df9634d7e376b3621c0682607e5a
SimenAsphaug <simenasphaug@hotmail.com>
2024-06-12 21:36:24 +0200

    test123

--- custom_components/discord_game/__init__.py
+++ custom_components/discord_game/__init__.py
@@ -1,42 +1,19 @@
-"""Component to integrate with Discord and get information about users online and game status."""
-from homeassistant import config_entries, core
-from homeassistant.const import Platform
-from homeassistant.helpers import config_validation as cv
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers.typing import ConfigType
+from .const import DOMAIN
 
-from .const import DOMAIN, DATA_HASS_CONFIG
-
-PLATFORMS = [Platform.SENSOR]
-
-CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
-
-
-async def async_setup_entry(
-        hass: core.HomeAssistant, entry: config_entries.ConfigEntry
-) -> bool:
-    """Set up platform from a ConfigEntry."""
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = entry.data
-
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, Platform.SENSOR)
-    )
-
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the Discord Game component."""
     return True
 
-
-async def async_unload_entry(
-        hass: core.HomeAssistant, entry: config_entries.ConfigEntry
-) -> bool:
-    """Unload a config entry."""
-    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
-        # Remove config entry from domain.
-        entry_data = hass.data[DOMAIN].pop(entry.entry_id)
-        # Remove options_update_listener.
-        entry_data["unsub_options_update_listener"]()
-
-    return unload_ok
-
-
-async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Discord Game from a config entry."""
     hass.data.setdefault(DOMAIN, {})
-    hass.data[DATA_HASS_CONFIG] = config
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    hass.data[DOMAIN].pop(entry.entry_id)
     return True
